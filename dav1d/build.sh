e2e2940ce969697284095272d762fd68c6eec6a6
Zachary Yedidia <zyedidia@gmail.com>
2024-11-21 23:46:14 +0000

    Add dav1d benchmark

---
+++ dav1d/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+meson setup build-lfi dav1d --cross-file ../toolchains/aarch64-lfi.txt -Ddefault_library=static
+ninja -C build-lfi
+
+meson setup build-lfi-stores dav1d --cross-file ../toolchains/aarch64-lfi-stores.txt -Ddefault_library=static
+ninja -C build-lfi-stores
+
+meson setup build-native dav1d --cross-file ../toolchains/aarch64-native.txt -Ddefault_library=static
+ninja -C build-native
---
+++ dav1d/download.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git clone https://github.com/zyedidia/dav1d -b lfi
