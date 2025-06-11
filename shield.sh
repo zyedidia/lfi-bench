6b5483280cbc78d85d4e11dc5f457fcb7bf9178d
Zachary Yedidia <zyedidia@gmail.com>
2025-06-11 11:24:21 -0700

    Update shield script to pin freq at 3GHz

--- shield.sh
+++ shield.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo cpufreq-set -c 5 -g performance
+sudo cpufreq-set -c 5 --min 3000MHz --max 3000MHz
+
 sudo cset shield -c 5 -k on
 sudo cset shield -e sudo -- -u "$USER" env "PATH=$PATH" bash
 sudo cset shield -c 5 -k off
+
+POLICYINFO=($(cpufreq-info -c 6 -p))
+echo "${POLICYINFO[0]}" "${POLICYINFO[1]}" "${POLICYINFO[2]}"
+sudo cpufreq-set -c 5 -g ${POLICYINFO[2]}
+sudo cpufreq-set -c 5 --min ${POLICYINFO[0]} --max ${POLICYINFO[1]}
