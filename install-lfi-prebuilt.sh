c6870e9c6c8b8822e644093103d7118770afdbf6
Zachary Yedidia <zyedidia@gmail.com>
2025-07-25 16:38:39 -0700

    Update prebuilt download script

--- install-lfi-prebuilt.sh
+++ install-lfi-prebuilt.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/zyedidia/lfi-llvm-toolchain/releases/download/prebuilt-toolchains/aarch64-lfi-clang.tar.gz
-wget https://github.com/zyedidia/lfi-llvm-toolchain/releases/download/prebuilt-toolchains/aarch64-lfi-stores-clang.tar.gz
-wget https://github.com/zyedidia/lfi-llvm-toolchain/releases/download/prebuilt-toolchains/aarch64-native-clang.tar.gz
+wget https://github.com/lfi-project/lfi-llvm-toolchain/releases/download/v0.9/aarch64-lfi-clang.tar.gz
+wget https://github.com/lfi-project/lfi-llvm-toolchain/releases/download/v0.9/aarch64-lfi-stores-clang.tar.gz
+wget https://github.com/lfi-project/lfi-llvm-toolchain/releases/download/v0.9/aarch64-native-clang.tar.gz
 
 tar -xf aarch64-lfi-clang.tar.gz
 tar -xf aarch64-lfi-stores-clang.tar.gz
