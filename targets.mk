85dde0211ba7370c5dc918dc50901ba70af933ff
Tal Garfinkel <talg@cs.stanford.edu>
2025-08-09 19:19:53 -0700

    migrated zstd and coremark to use cmake and target template.

--- targets.mk
+++ targets.mk
@@ -6,27 +6,29 @@
 #
 # Optional variables:
 #   CMAKE_FLAGS - Project-specific CMake flags (default: empty)
+#   CMAKE_SOURCE_DIR - CMake source directory (default: $(TARGET_DIR))
 #   CMAKE_INSTALL_PREFIX_LFI - LFI install prefix (default: --install-prefix $(LFI_INSTALL))
 #   CMAKE_INSTALL_PREFIX_STORES - LFI-stores install prefix (default: --install-prefix $(STORES_INSTALL))  
 #   CMAKE_INSTALL_PREFIX_NATIVE - Native install prefix (default: --install-prefix $(NATIVE_INSTALL))
 
 # Set default values if not specified
 CMAKE_FLAGS ?=
+CMAKE_SOURCE_DIR ?= $(TARGET_DIR)
 CMAKE_INSTALL_PREFIX_LFI ?= --install-prefix $(LFI_INSTALL)
 CMAKE_INSTALL_PREFIX_STORES ?= --install-prefix $(STORES_INSTALL)
 CMAKE_INSTALL_PREFIX_NATIVE ?= --install-prefix $(NATIVE_INSTALL)
 
 # Common build targets
 build-lfi: $(TARGET_DIR)
-	cmake -S $(TARGET_DIR) $(CMAKE_FLAGS) -B build-lfi -DCMAKE_TOOLCHAIN_FILE=$(LFI_TOOLCHAIN) $(CMAKE_INSTALL_PREFIX_LFI) -DCMAKE_BUILD_TYPE=Release
+	cmake -S $(CMAKE_SOURCE_DIR) $(CMAKE_FLAGS) -B build-lfi -DCMAKE_TOOLCHAIN_FILE=$(LFI_TOOLCHAIN) $(CMAKE_INSTALL_PREFIX_LFI) -DCMAKE_BUILD_TYPE=Release
 	cmake --build build-lfi -j $(JOBS)
 
 build-lfi-stores: $(TARGET_DIR)
-	cmake -S $(TARGET_DIR) $(CMAKE_FLAGS) -B build-lfi-stores -DCMAKE_TOOLCHAIN_FILE=$(STORES_TOOLCHAIN) $(CMAKE_INSTALL_PREFIX_STORES) -DCMAKE_BUILD_TYPE=Release
+	cmake -S $(CMAKE_SOURCE_DIR) $(CMAKE_FLAGS) -B build-lfi-stores -DCMAKE_TOOLCHAIN_FILE=$(STORES_TOOLCHAIN) $(CMAKE_INSTALL_PREFIX_STORES) -DCMAKE_BUILD_TYPE=Release
 	cmake --build build-lfi-stores -j $(JOBS)
 
 build-native: $(TARGET_DIR)
-	cmake -S $(TARGET_DIR) $(CMAKE_FLAGS) -B build-native -DCMAKE_TOOLCHAIN_FILE=$(NATIVE_TOOLCHAIN) $(CMAKE_INSTALL_PREFIX_NATIVE) -DCMAKE_BUILD_TYPE=Release
+	cmake -S $(CMAKE_SOURCE_DIR) $(CMAKE_FLAGS) -B build-native -DCMAKE_TOOLCHAIN_FILE=$(NATIVE_TOOLCHAIN) $(CMAKE_INSTALL_PREFIX_NATIVE) -DCMAKE_BUILD_TYPE=Release
 	cmake --build build-native -j $(JOBS)
 
 # Common install target
